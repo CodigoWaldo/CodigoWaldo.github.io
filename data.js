a0713eb0b65f1af1f3762adc92b8f8971cccbf1b
W a l d o <70487661+CodigoWaldo@users.noreply.github.com>
2023-07-16 21:53:59 -0300

    update

---
+++ data.js
@@ -0,0 +1,40 @@
+const main = {
+  name: 'Walter Voegeli',
+  mail: '',
+  img: 'Images/waldo.jpg',
+  role: ['Estudiante de ingenieria informática.'],
+  role2: ['Técnico constructor nacional.'],
+
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [  
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/walter-voegeli-650b78211/'
+    },  
+    {
+      name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://www.instagram.com/waldobook/'
+    },   
+    {
+      name: 'YouTube',
+      iconName: 'logo-youtube',
+      link: 'https://www.youtube.com/@codigowaldo'
+    },  
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/CodigoWaldo'
+    },    
+    {
+      name: 'SourceForge',
+      iconName: 'flame-outline',
+      link: 'https://sourceforge.net/u/waldovoe/profile'
+    },     
+  ],
+  links: [
+    { name: 'Experimentos', link: 'Stuff/Lab.html' }
+  ]
+};
+
