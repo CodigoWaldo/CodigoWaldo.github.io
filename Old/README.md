faddb3a671acec9cacf96fba2ae842014ef5c0ce
Walter Voegeli <70487661+CodigoWaldo@users.noreply.github.com>
2025-06-20 15:33:18 -0300

    cumple

---
+++ Old/README.md
@@ -0,0 +1,12 @@
+# WEB Developing
+EN</br>
+Web site made for learn and test, also i use this space as my personal web. </br>
+[link to the site](https://codigowaldo.github.io)</br></br>
+ES</br>
+Sitio web creado para aprender y probar, también utilizo este espacio como mi sitio web personal.</br>
+[link al sitio](https://codigowaldo.github.io)</br>
+
+```
+Code: HTML - CSS - JS
+```
+
---
+++ Old/data.js
@@ -0,0 +1,35 @@
+const main = {
+  name: 'Walter Voegeli',
+  mail: '',
+  img: 'Images/waldo.jpg',
+  role: ['Estudiante de ingenieria informática.'],
+  role2: ['Técnico constructor nacional.'],
+
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [  
+    {
+      name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://www.instagram.com/waldobook/'
+    },   
+    {
+      name: 'YouTube',
+      iconName: 'logo-youtube',
+      link: 'https://www.youtube.com/@codigowaldo'
+    },  
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/CodigoWaldo'
+    },    
+    {
+      name: 'SourceForge',
+      iconName: 'flame-outline',
+      link: 'https://sourceforge.net/u/waldovoe/profile'
+    },     
+  ],
+  links: [
+    { name: 'Experimentos', link: 'Stuff/Lab.html' }
+  ]
+};
+
